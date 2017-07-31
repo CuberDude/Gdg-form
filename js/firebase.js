9403007b6afda17fa625669668ead2fd9da09a27
Gaurav Jain <crazcuber@gmail.com>
2017-07-31 16:19:07 +0530

    demo

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDqV2qLPdRtCLaTX9GMa5qRMkm_sXcBHpg",
+    authDomain: "gdg-form.firebaseapp.com",
+    databaseURL: "https://gdg-form.firebaseio.com",
+    projectId: "gdg-form",
+    storageBucket: "",
+    messagingSenderId: "999376873410"
+};
+firebase.initializeApp(config);
